c5aa42e1b91942f8bfb2134383204657e05a8fbb
Marcelo Reyna <Lepozepo@gmail.com>
2014-08-22 08:57:01 -0700

    Added C.upload

--- client/functions.js
+++ client/functions.js
@@ -20,6 +20,26 @@ C = {
 
 			reader.readAsArrayBuffer(file);
 		});
+	},
+	upload: function(files,callback){
+		var options = {};
 
+		_.each(files,function(file){
+			var reader = new FileReader;
+
+			reader.onload = function () {
+				options.db_id = _cloudinary.insert({});
+				Meteor.call("cloudinary_upload",reader.result,options,function(err,res){
+					if(err){
+						_cloudinary.remove(options.db_id);
+						console.log(err);
+					} else {
+						callback && callback(res);
+					}
+				});
+			};
+
+			reader.readAsDataURL(file);
+		});
 	}
 }
